159b496e58b3d3ae5c090aed8036417f92138bf2
Samit Kapoor <samit@aegiscovenant.com>
2023-07-10 08:46:30 +0000

    Hiding Railway credentials

--- handlers/connection.mjs
+++ handlers/connection.mjs
@@ -1,15 +1,17 @@
 import mysql2 from "mysql2";
 
+import { _database, _host, _port, _password, _user } from "./env";
+
 // used to connect to the database
 export const getConnection = () => {
   console.log("CONNECTING TO THE DATABASE...");
   const connection = mysql2
     .createConnection({
-      host: "containers-us-west-146.railway.app",
-      user: "root",
-      password: "OnlrF2AiReeS9LTLi1GI",
-      database: "railway",
-      port: 7256,
+      host: _host,
+      user: _user,
+      password: _password,
+      database: _database,
+      port: _port
     })
     .promise();
   console.log("CONNECTED TO THE DATABASE!");
